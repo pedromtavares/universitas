6c27a80276cf280a3b04124df0853bb2c492c93b
Pedro <pedromateustavares@gmail.com>
2011-02-19 18:15:38 -0300

    changing upload cache folder for heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,6 @@
 CarrierWave.configure do |config|
 	config.storage = :s3
-	puts ENV['S3KEY']
-	puts ENV['S3SECRET']
+	config.cache_dir = "#{Rails.root}/tmp/uploads"
 	config.s3_access_key_id = ENV['S3KEY']
   config.s3_secret_access_key = ENV['S3SECRET']
   config.s3_bucket = 'universitas'
