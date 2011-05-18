6040088cb39df972b04735d07552c45403649632
Pedro <pedromateustavares@gmail.com>
2011-05-18 04:10:13 -0300

    adding default url host on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,8 @@ Universitas::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+	config.action_mailer.default_url_options = { :host => 'universit.as' }
+
   # Enable threaded mode
   # config.threadsafe!
 
