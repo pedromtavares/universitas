1f72a266839048b1634fec66d02b3a956912236e
Pedro <pedromateustavares@gmail.com>
2011-06-08 02:10:08 -0300

    refactored some view code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,29 @@ module ApplicationHelper
 	def full_page?
 		!user_signed_in? && controller.controller_name != 'home'
 	end
+	
+	def submit_button(small = false, options = {}, &block)
+		klass = 'button '
+		klass += ' small-button ' if small
+		klass += options.delete(:class) if options[:class]
+		content_tag(:button, {:type => 'submit', :class => klass}.merge(options), &block)
+	end
+	
+	def link_to_icon(icon, title, *args)
+		name = case icon
+		when :add
+			"plusthick"
+		when :edit
+			"pencil"
+		when :remove
+			"closethick"
+		when :user
+			"person"
+		when :check
+			"check"
+		end
+		link_to(*args) do
+			content_tag(:span, "", :class => "ui-icon ui-icon-#{name}", :title => title)
+		end
+	end
 end
