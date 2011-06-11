93c094d73ba08fb8abe15cf14338996569df8bd3
Pedro <pedromateustavares@gmail.com>
2011-06-10 22:30:34 -0300

    fixing textile helper to open in a new window

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,7 +6,7 @@ module PostsHelper
 	end
 	
 	def textile_guide
-		content_tag(:a, :href => "javascript:window.open('#{textile_path}','popup_window','height=400,width=600')") do
+		content_tag(:a, :href => "javascript:window.open('#{textile_path}','popup_window','height=400,width=600')", :target => '_blank') do
 			t('topics.textile')
 		end
 	end
