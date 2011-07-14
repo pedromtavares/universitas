4f6567ec89d2a641b9aa1399b496a42c52160e17
Pedro Mateus Tavares <pedromateustavares@gmail.com>
2011-07-14 15:53:29 -0300

    fixing yaml parser error and removing new engine from boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,8 +1,3 @@
-# this is required for running on Linode instance that hosts Universitas
-if ENV['RAILS_ENV'] == "production"
-  require 'yaml'
-  YAML::ENGINE.yamler= 'syck'
-end
 require 'rubygems'
 
 # Set up gems listed in the Gemfile.
