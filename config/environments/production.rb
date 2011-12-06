1536c8e880b76bcb425004056d6f99afa24d526e
Pedro Mateus Tavares <pedro@pedromtavares.com>
2011-12-06 02:08:53 -0300

    attempt to fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,10 +48,13 @@ Universitas::Application.configure do
   
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  config.active_support.deprecation = :notify
 end
