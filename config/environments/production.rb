0a5c9225291dc9c3338c5aaf40d6cca454d5e7de
Pedro Mateus Tavares <pedro@pedromtavares.com>
2011-12-06 02:54:29 -0300

    adding files to be precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,13 +48,14 @@ Universitas::Application.configure do
   
   # Compress JavaScripts and CSS
   config.assets.compress = true
-  
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  config.assets.precompile += ['history.js', 'theme/ie.css']
   
   config.active_support.deprecation = :notify
 end
