ed455e67613b1633dad5bbaa7568c0c061699099
pedromtavares <pedro@pedromtavares.com>
2011-12-08 22:52:31 -0300

    testing if github links my account

--- app/mailers/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer.rb
@@ -1,6 +1,5 @@
 class PasswordResetMailer < Devise::Mailer
   def reset_password_instructions(record)
-    p "enqueued\n\n\n\n\n\n\n\n"
     Resque.enqueue(EmailSender, :password, record.id)
   end
 end
